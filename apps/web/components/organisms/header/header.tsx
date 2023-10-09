a9b16137fcb70b663ac1e0a53cc6b3ee79bf5c86
Artyom Novoselov <an@versta24.ru>
2023-10-09 21:44:51 +0400

    feat: add upload button to header

--- apps/web/components/organisms/header/header.tsx
+++ apps/web/components/organisms/header/header.tsx
@@ -1,10 +1,12 @@
 import Logo from "@web/components/atoms/logo";
 import s from "./header.module.css";
+import UploadButton from "@web/components/molecules/upload-button";
 
 export default function Header() {
   return (
     <header className={s.header}>
       <Logo />
+      <UploadButton />
     </header>
   );
 }
