b1a39816960f76720bcad5605723aed31b1baabb
Artyom Novoselov <an@versta24.ru>
2023-10-09 23:35:24 +0400

    fix: no dynamic import for icons

--- apps/web/components/atoms/icon/icon.tsx
+++ apps/web/components/atoms/icon/icon.tsx
@@ -1,4 +1,9 @@
 import s from "./icon.module.css";
+import Cloud from "@web/assets/icons/cloud.svg";
+import Close from "@web/assets/icons/close.svg";
+import Download from "@web/assets/icons/download.svg";
+import Edit from "@web/assets/icons/edit.svg";
+import Trash from "@web/assets/icons/trash.svg";
 
 type IconName = "close" | "cloud" | "download" | "edit" | "trash";
 type IconColor = "yellow" | "black" | "gray";
@@ -8,9 +13,16 @@ interface IconProps {
   icon: IconName;
 }
 
-export default async function Icon({ color, icon }: IconProps) {
-  const Icon = await import(`@web/assets/icons/${icon}.svg`).then(
-    icon => icon.default
-  );
-  return <Icon className={s[color]} />;
+const ICONS: Record<IconName, any> = {
+  cloud: Cloud,
+  close: Close,
+  download: Download,
+  edit: Edit,
+  trash: Trash,
+};
+
+export default function Icon({ color, icon }: IconProps) {
+  const Icon = ICONS[icon];
+
+  return <Icon className={s[color ?? ""]} />;
 }
