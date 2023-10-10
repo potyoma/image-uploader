158dfea3b924db9991d387e29a0125f126b58e13
Artyom Novoselov <an@versta24.ru>
2023-10-10 18:50:14 +0400

    feat: heading component

---
+++ apps/web/components/atoms/heading/heading.tsx
@@ -0,0 +1,26 @@
+import clsx from "clsx";
+import { DetailedHTMLProps, HTMLAttributes } from "react";
+import s from "./heading.module.css";
+
+type HeadingLevel = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+
+type HeadingProps = DetailedHTMLProps<
+  HTMLAttributes<HTMLHeadingElement>,
+  HTMLHeadingElement
+> & {
+  level?: HeadingLevel;
+};
+
+export default function Heading({
+  level = "h3",
+  className,
+  children,
+  ...rest
+}: HeadingProps) {
+  const Component = level;
+  return (
+    <Component className={clsx(s.heading, s[level], className)} {...rest}>
+      {children}
+    </Component>
+  );
+}
