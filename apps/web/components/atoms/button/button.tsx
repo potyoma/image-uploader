b7a6e3cf2385b9cc5e3ff7e0250b1e31db8e26f2
Artyom Novoselov <an@versta24.ru>
2023-10-10 18:49:48 +0400

    feat: add transparent button variant

--- apps/web/components/atoms/button/button.tsx
+++ apps/web/components/atoms/button/button.tsx
@@ -5,15 +5,25 @@ import s from "./button.module.css";
 type ButtonProps = DetailedHTMLProps<
   ButtonHTMLAttributes<HTMLButtonElement>,
   HTMLButtonElement
->;
+> & {
+  transparent?: boolean;
+};
 
 export default function Button({
   className,
   children,
+  transparent,
   ...restProps
 }: ButtonProps) {
   return (
-    <button className={clsx(s.button, className)} {...restProps}>
+    <button
+      className={clsx(
+        s.button,
+        transparent ? s.transparent : s.buttonBg,
+        className
+      )}
+      {...restProps}
+    >
       {children}
     </button>
   );
