223d7aadb9f36c747228205d88ae3ce4b0a68281
Artyom Novoselov <an@versta24.ru>
2023-10-10 18:56:46 +0400

    feat: image block component

---
+++ apps/web/components/organisms/image-block/image-block.tsx
@@ -0,0 +1,30 @@
+import ImageHeading from "@web/components/molecules/image-heading/image-heading";
+import { Pic } from "@web/stab/pics";
+import Image from "../image/image";
+import s from "./image-block.module.css";
+
+interface ImageBlockProps {
+  date: string;
+  pics: Pic[];
+}
+
+export default function ImageBlock({ date, pics }: ImageBlockProps) {
+  const count = pics?.length ?? 0;
+
+  return (
+    <div>
+      <ImageHeading date={date} count={count} />
+      <div className={s.images}>
+        {pics.map(pic => (
+          <Image
+            key={(date + pic.date + pic.alt).replace(" ", "_").toLowerCase()}
+            date={pic.date}
+            alt={pic.alt}
+            comment={pic.comment}
+            src={pic.src}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
