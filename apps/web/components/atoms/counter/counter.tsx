d1de76475788ff9c372145cafcf7ede9158864ff
Artyom Novoselov <an@versta24.ru>
2023-10-11 16:12:13 +0400

    fix: spacing in logo and counter

--- apps/web/components/atoms/counter/counter.tsx
+++ apps/web/components/atoms/counter/counter.tsx
@@ -1,9 +1,12 @@
 "use client";
 import { useImageKeeperStore } from "@web/store/context";
-import Text from "../text/text";
+import Text from "../text";
+import s from "./counter.module.css";
 
 export default function Counter() {
   const { countPictures } = useImageKeeperStore();
 
-  return <Text>{countPictures} images stored in keeper</Text>;
+  return (
+    <Text className={s.text}>{countPictures} images stored in keeper</Text>
+  );
 }
